61adf85829c22626e6b4b7a317d2ec0d55266fc7
FFFHHHJJJ <106230239+FFFHHHJJJ@users.noreply.github.com>
2022-09-04 21:43:11 +0800

    修改main分支

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,14 @@
 <template>
-    <div>
+  <div>
     的点点滴滴
     <h1>表演</h1>
     <h2>22222</h2>
-     <h1>表演</h1>
-    </div>
+    <h1>表演</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {}
 </script>
 
-<style>
-    
-</style>
+<style></style>
